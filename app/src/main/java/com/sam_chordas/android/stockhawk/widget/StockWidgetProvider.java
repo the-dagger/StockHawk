44d257033fd5721f6f8b59be84504f5b8bda8365
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-10-29 21:03:16 +0530

    clean: cleaned up code and naming

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
@@ -21,44 +21,28 @@ public class StockWidgetProvider extends AppWidgetProvider {
 
     private static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                         int appWidgetId) {
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
-        // Instruct the widget manager to update the widget
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            setRemoteAdapter(context, views);
+            setRemoteAdapter(context, remoteViews);
         } else {
-            setRemoteAdapterV11(context, views);
+            setRemoteAdapterV11(context, remoteViews);
         }
         Intent launchMain = new Intent(context, MyStocksActivity.class);
-//        Intent launchDetail = new Intent(context, StockDetailActivity.class);
         PendingIntent pendingMainIntent = PendingIntent.getActivity(context, 0, launchMain, 0);
-//        PendingIntent pendingDetailIntent = TaskStackBuilder.create(context)
-//                .addNextIntentWithParentStack(launchDetail)
-//                .getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.widget_toolbar, pendingMainIntent);
-//        views.setOnClickPendingIntent(R.id.widget_listView,pendingDetailIntent);
-        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId,R.id.widget_listView);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        remoteViews.setOnClickPendingIntent(R.id.widget_toolbar, pendingMainIntent);
+
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_listView);
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
 
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
